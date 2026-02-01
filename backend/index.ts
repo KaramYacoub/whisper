0fa6eb09785b72947ac10972e1035fa5f76f4452
KaramYacoub <karamarandas03@gmail.com>
2026-02-01 20:59:53 +0300

    feat: implement socket server initialization and user authentication

--- backend/index.ts
+++ backend/index.ts
@@ -3,12 +3,18 @@ dns.setServers(["1.1.1.1"]);
 
 import app from "./src/app";
 import { connectDB } from "./src/config/database";
+import { createServer } from "node:http";
+import { initializeSocket } from "./src/utils/socket";
 
 const PORT = process.env.PORT || 3000;
 
+const httpServer = createServer(app);
+
+initializeSocket(httpServer);
+
 connectDB()
   .then(() => {
-    app.listen(PORT, () => {
+    httpServer.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
